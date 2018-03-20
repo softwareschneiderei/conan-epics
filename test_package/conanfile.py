b0769156131ea89e677eed3730ba6c91d4e4c154
Matt Clarke <matt.clarke@esss.se>
2018-03-20 09:22:21 +0100

    Update conanfile.py

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,4 +1,4 @@
-from conans import ConanFile, CMake, tools
+from conans import ConanFile, CMake, tools, RunEnvironment
 import os
 
 
@@ -15,14 +15,12 @@ class EpicsbaseTestConan(ConanFile):
 
     def imports(self):
         self.copy("*.dll", dst="bin", src="bin")
+        self.copy("*.lib", dst="bin", src="lib")
         self.copy("*.dylib*", dst="bin", src="lib")
         self.copy("*.so*", dst="bin", src="lib")
 
     def test(self):
         os.chdir("bin")
-
-        if tools.os_info.is_linux:
-            ld_library_path = os.environ.get("LD_LIBRARY_PATH", "")
-            os.environ["LD_LIBRARY_PATH"] = ld_library_path + ":."
-
-        self.run(".%sexample" % os.sep)
+        env_build = RunEnvironment(self)
+        with tools.environment_append(env_build.vars):
+            self.run(".%sexample" % os.sep)
