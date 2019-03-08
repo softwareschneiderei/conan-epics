8c1646f009b2a15943df13a7b4d503d4e3305013
Jonas Nilsson <jonas.nilsson@esss.se>
2019-03-08 15:24:05 +0100

    Fix finding of libraries.

--- FindEPICS.cmake
+++ FindEPICS.cmake
@@ -1,17 +1,20 @@
-find_path(EPICS_INCLUDE_DIR NAMES epicsVersion.h PATHS ${CONAN_INCLUDE_DIRS_EPICS})
-find_path(EPICS_INCLUDE_OS_DIR NAMES osdEvent.h PATHS ${EPICS_INCLUDE_DIR}/os/Linux ${EPICS_INCLUDE_DIR}/os/Darwin)
-find_path(EPICS_INCLUDE_COMP_DIR NAMES compilerSpecific.h PATHS ${EPICS_INCLUDE_DIR}/compiler/gcc ${EPICS_INCLUDE_DIR}/compiler/clang)
+find_path(EPICS_INCLUDE_DIR NAMES epicsVersion.h)
+find_path(EPICS_INCLUDE_OS_DIR NAMES osdEvent.h HINTS ${EPICS_INCLUDE_DIR}/os/Linux ${EPICS_INCLUDE_DIR}/os/Darwin)
+find_path(EPICS_INCLUDE_COMP_DIR NAMES compilerSpecific.h HINTS ${EPICS_INCLUDE_DIR}/compiler/gcc ${EPICS_INCLUDE_DIR}/compiler/clang)
 
 set(EPICS_INCLUDE_DIR ${EPICS_INCLUDE_DIR} ${EPICS_INCLUDE_OS_DIR} ${EPICS_INCLUDE_COMP_DIR})
 
-find_library(EPICS_LIBRARY NAMES ${CONAN_LIBS_EPICS} PATHS ${CONAN_LIB_DIRS_EPICS})
+set(EPICS_LIB_FILES Com ca dbCore dbRecStd nt pvAccess pvAccessCA pvAccessIOC pvData pvDatabase pvaClient qsrv)
 
-set(EPICS_FOUND TRUE)  
-set(EPICS_LIBRARIES ${EPICS_LIBRARY})
+foreach(CUR_NAME ${EPICS_LIB_FILES})
+  find_library(FOUND_LIB${CUR_NAME} NAMES ${CUR_NAME} HINTS ${CONAN_LIB_DIRS_EPICS})
+  set(EPICS_LIBRARIES ${EPICS_LIBRARIES} ${FOUND_LIB${CUR_NAME}})
+endforeach(CUR_NAME)
+
+set(EPICS_FOUND TRUE)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(EPICS DEFAULT_MSG
     EPICS_INCLUDE_DIR
     EPICS_LIBRARIES
-    EPICS_LIBRARY
 )
